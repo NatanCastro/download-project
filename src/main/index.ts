bdf21a5fcd18bdefc3e84f81f9a7d0ec91cba984
Natan Gabriel Almeida de castro <castro.natan@outlook.com>
2024-07-06 21:34:20 -0300

    Electron (#2)
    * chore: restart
    
    * chore: init electron project
    
    * chore: change base electron base template
    
    * chore(): clean package.json file
    
    * chore: update pnpm-lock.json
    
    * feat: v1
    
    * feat: add release workflow
    
    * chore: add ?asset to image import

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,7 @@
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png'
+import icon from '../../resources/icon.png?asset'
 import { DownloaderFactory } from './downloader/downloaders/downloader_factory'
 import { DownloadManager } from './downloader/managers/manager'
 
